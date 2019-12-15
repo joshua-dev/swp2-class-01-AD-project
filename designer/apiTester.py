a12d54aa56b68266b93e07e1827434b6ff473fe2
joshua-dev <msh01175@gmail.com>
2019-12-15 16:26:44 +0900

    Success API test

--- designer/apiTester.py
+++ designer/apiTester.py
@@ -1,19 +1,11 @@
-import firebase_admin
-from firebase_admin import credentials
 import requests
-import time
-
-cred = credentials.Certificate(
-    './kcc-library-firebase-adminsdk-tadou-6febe202b4.json')
-firebase_admin.initialize_app(cred)
-
-Elapsed_time = time.time()
 
 res = requests.get(
     'https://us-central1-kcc-library.cloudfunctions.net/showAll')
 
-print(res.status_code)
+result = res.json()
 
-Elapsed_time = time.time() - Elapsed_time
-
-print(f'Elapsed Time: {Elapsed_time}s')
+for key in result.keys():
+    val = result[key]
+    for k in val.keys():
+        print(f'{k}: {val[k]}')
